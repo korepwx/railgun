017c8d2293739d7936b6eae80c679053e76f58f0
Dawei Yang <davy.pristina@gmail.com>
2014-08-09 00:14:08 +0800

    Add test page. Comment docs.css and railgun.css to fix se7en display displacement.

--- railgun/website/views.py
+++ railgun/website/views.py
@@ -34,6 +34,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/test/')
+def test_page():
+    return render_template('test.html')
+
+
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
