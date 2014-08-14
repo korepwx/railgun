661f0d5cdaf501f57b5610605d7e30fedbe98ac4
Korepwx <public@korepwx.com>
2014-08-14 22:35:57 +0800

    Print website log into logs/website.log

--- website.py
+++ website.py
@@ -8,7 +8,16 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This file is released under BSD 2-clause license.
 
+import logging
+from logging import RotatingFileHandler
 from railgun.website.context import app
 
+
+def init_log(app):
+    handler = RotatingFileHandler('logs/website.log')
+    handler.setLevel(logging.WARNING)
+    app.logger.addHandler(handler)
+
 if (__name__ == '__main__'):
+    init_log(app)
     app.run()
