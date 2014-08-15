20ac8d2d9a8b9a2476d9aad133f8b2fe17d40a1c
Dawei Yang <davy.pristina@gmail.com>
2014-08-15 16:13:51 +0800

    Delete obselete files and setup.

--- railgun/website/ldapconfig.py
+++ railgun/website/ldapconfig.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# @file: railgun/website/__init__.py
+# @file: entry/website/webconfig.py
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Contributors:
-#   public@korepwx.com   <public@korepwx.com>
 #   Dawei Yang           <davy.pristina@gmail.com>
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This file is released under BSD 2-clause license.
 
-from . import api, codelang, context, credential, forms, hw, i18n, \
-    models, navibar, scriptlibs, renders, utility, views, webconfig, \
-    ldapconfig
+import os
+import ldap
+from config import *
+
+LDAP_ADMIN_KEY = (
+    open(os.path.join(ENTRY_ROOT, 'keys/ldapKey.txt'), 'rb').read().strip()
+)
+
+BASE_DN = 'ou=People,dc=secoder,dc=net'
+ADMIN_DN = 'cn=admin,dc=secoder,dc=net'
+
+CONN = ldap.initialize('ldap://ldap.secoder.net')
+CONN.simple_bind_s(ADMIN_DN, LDAP_ADMIN_KEY)
